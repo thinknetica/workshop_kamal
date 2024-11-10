0ff05944a6dd49c715943be010a5268297f8c3f8
Alexandr Borisov <ab@cifronomika.ru>
2024-11-10 18:48:42 +0000

    Add missing block

--- config/initializers/yabeda.rb
+++ config/initializers/yabeda.rb
@@ -1,8 +1,10 @@
-Yabeda::ActiveJob.install!
+Yabeda.configure do
+  Yabeda::ActiveJob.install!
 
-if ENV.fetch("START_METRICS_SERVER", nil)
-  require "webrick"
-  require "rackup/handler/webrick" # Explicitly require the WEBrick handler
+  if ENV.fetch("START_METRICS_SERVER", nil)
+    require "webrick"
+    require "rackup/handler/webrick" # Explicitly require the WEBrick handler
 
-  Yabeda::Prometheus::Exporter.start_metrics_server!(rack_handler: :puma)
+    Yabeda::Prometheus::Exporter.start_metrics_server!(rack_handler: :puma)
+  end
 end
