e452fea89bf050997c511bc32910288cfed75893
Alexandr Borisov <ab@cifronomika.ru>
2024-11-10 18:53:23 +0000

    Add datastore

--- config/initializers/yabeda.rb
+++ config/initializers/yabeda.rb
@@ -2,6 +2,8 @@ Yabeda.configure do
   Yabeda::ActiveJob.install!
 
   if ENV.fetch("START_METRICS_SERVER", nil)
+    Prometheus::Client.config.data_store = Prometheus::Client::DataStores::DirectFileStore.new(dir: "/tmp")
+
     require "webrick"
     require "rackup/handler/webrick" # Explicitly require the WEBrick handler
 
