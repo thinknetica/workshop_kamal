6b722af25431059a318bae6ffc48815dd564e7c4
Alexandr Borisov <ab@cifronomika.ru>
2024-11-10 18:33:53 +0000

    Fix metrics server for jobs

--- config/initializers/yabeda.rb
+++ config/initializers/yabeda.rb
@@ -1,2 +1,8 @@
 Yabeda::ActiveJob.install!
-Yabeda::Prometheus::Exporter.start_metrics_server! if ENV.fetch("START_METRICS_SERVER", nil)
+
+if ENV.fetch("START_METRICS_SERVER", nil)
+  require "webrick"
+  require "rackup/handler/webrick" # Explicitly require the WEBrick handler
+
+  Yabeda::Prometheus::Exporter.start_metrics_server!(rack_handler: :puma)
+end
